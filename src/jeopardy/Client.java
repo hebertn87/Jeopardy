e127c49c87c7447536789c0d0ca1f233e61cfe30
Nathan <hebertn87@gmail.com>
2018-03-22 13:26:44 -0600

    Pushed all files

--- src/jeopardy/Client.java
+++ src/jeopardy/Client.java
@@ -7,32 +7,39 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
 import java.io.DataOutputStream;
+import java.io.OutputStream;
 
 //Client class handles connection to server
 public class Client {
 	public static void main(String args[]) throws IOException {
+		
+		int clientNum = 1;
 		//Establish connection through port 1286
 		Socket s = new Socket("localhost",1286);
 		
 		//Access the input stream with data input stream
 		InputStream sIn = s.getInputStream();
+		OutputStream sOn = s.getOutputStream();
 		
 		//Access the input stream with data input stream
 		DataInputStream socketDIS = new DataInputStream(sIn);
+		DataOutputStream socketDOS = new DataOutputStream(sOn);
 		
 		//Read from socket data input stream
-		String testString = new String (socketDIS.readUTF());
+		//String testString = new String (socketDIS.readUTF());
 		
 		//Print data read
-		System.out.println(testString);
+		socketDOS.writeInt(clientNum);
+		
+		socketDOS.writeUTF("I receieved your message, I'm ready to play!");
 		
 		//clean up
 		socketDIS.close();
+		socketDOS.close();
 		sIn.close();
+		sOn.close();
 		s.close();
-	}
-	
-	
+	}	
 }
 
 
--- src/jeopardy/Server.java
+++ src/jeopardy/Server.java
@@ -1,5 +1,5 @@
 //Nathan Hebert 3/7/2018
-//This is the server that allows the clients to connet to the game
+//This is the server that allows the clients to connect to the game
 package jeopardy;
 
 import java.io.DataOutputStream;
@@ -7,23 +7,37 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.io.InputStream;
+import java.io.DataInputStream;
 
-public class Server {
+public class Server implements Runnable {
+	
+	Socket s;
+	
+	public Server(Socket _s) {
+		this.s = _s;
+	}
+	
 	public static void main(String args[]) throws IOException {
 		
 		ServerSocket ss = new ServerSocket(1286);
-		Socket s = ss.accept();
+		System.out.println("Server is listening for clients.");
 		
-		OutputStream socketOutStream = s.getOutputStream();
-		DataOutputStream socketDOS = new DataOutputStream(socketOutStream);
-		
-		//Communicate with the socket 
-		socketDOS.writeUTF("Welcome To Jeopardy!");
-		
-		//Clean up
-		socketDOS.close();
-		socketOutStream.close();
-		s.close();
-		ss.close();
+		while(true) {
+			Socket sock = ss.accept();
+			InputStream inStream = sock.getInputStream();
+			DataInputStream serverInStream = new DataInputStream(inStream);
+			Integer clientNum = new Integer (serverInStream.readInt());
+			Integer leftToConn = 3 - clientNum;
+			System.out.println("Client " + clientNum + " has connected. Waiting on " +  leftToConn + " Players.");
+			new Thread(new Server(sock)).start();
+		}
 	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
